a9d8d77e01bf1a84aa76fa91077503cd502dbf7e
dgarzong <dgarzong@ucundinamarca.edu.co>
2021-03-22 20:52:53 -0500

    Adiciono sql para hacer pruebas de usuario

--- UdeC-Educa_JPA/src/main/java/com/udeceduca/DTO/UserDTO.java
+++ UdeC-Educa_JPA/src/main/java/com/udeceduca/DTO/UserDTO.java
@@ -5,9 +5,12 @@
  */
 package com.udeceduca.DTO;
 
+//Data Transfer Object 
+//Carries data generated a POJO
+
 /**
  *
- * @author Sammy Guergachi <sguergachi at gmail.com>
+ * @author UdeC-Educa Dev's Team
  */
 public class UserDTO {
 
@@ -31,6 +34,69 @@ public class UserDTO {
         this.password = password;
     }
 
+    public String getIdentification() {
+        return identification;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public String getSecond_name() {
+        return second_name;
+    }
+
+    public String getFirst_lastname() {
+        return first_lastname;
+    }
+
+    public String getSecond_lastname() {
+        return second_lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setIdentification(String identification) {
+        this.identification = identification;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public void setSecond_name(String second_name) {
+        this.second_name = second_name;
+    }
+
+    public void setFirst_lastname(String first_lastname) {
+        this.first_lastname = first_lastname;
+    }
+
+    public void setSecond_lastname(String second_lastname) {
+        this.second_lastname = second_lastname;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
     
     
 
